5320c75ccda4bdd432831253eabdba17e3287e26
Alberto Silva <asilgag@gmail.com>
2021-11-14 08:52:31 +0100

    Remove src/http/public assets from repository and install them with a yarn command

--- scripts/copy-http-files-to-src.js
+++ scripts/copy-http-files-to-src.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 // Copy files from Bulma to assets directory
 const bulmaSrcDir = 'node_modules/bulma/css/';
-const bulmaTargetDir = `${process.env.INIT_CWD}/lib/http/public/assets/bulma/`;
+const bulmaTargetDir = `${process.env.INIT_CWD}/src/http/public/assets/bulma/`;
 fs.mkdirSync(bulmaTargetDir, { recursive: true });
 fs.copyFileSync(
   `${bulmaSrcDir}/bulma.min.css`,
@@ -24,7 +24,7 @@ fs.copyFileSync(
 
 // Copy files from Clipboard to assets directory
 const clipboardSrcDir = 'node_modules/clipboard/dist/';
-const clipboardTargetDir = `${process.env.INIT_CWD}/lib/http/public/assets/clipboard/`;
+const clipboardTargetDir = `${process.env.INIT_CWD}/src/http/public/assets/clipboard/`;
 fs.mkdirSync(clipboardTargetDir, { recursive: true });
 fs.copyFileSync(
   `${clipboardSrcDir}/clipboard.min.js`,
@@ -33,7 +33,7 @@ fs.copyFileSync(
 
 // Copy files from datatables to assets directory
 const dataTablesSrcDir = 'node_modules/simple-datatables/dist/';
-const dataTablesTargetDir = `${process.env.INIT_CWD}/lib/http/public/assets/simple-datatables/`;
+const dataTablesTargetDir = `${process.env.INIT_CWD}/src/http/public/assets/simple-datatables/`;
 fs.mkdirSync(dataTablesTargetDir, { recursive: true });
 fs.copyFileSync(
   `${dataTablesSrcDir}/umd/simple-datatables.js`,
