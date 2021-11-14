d3cf8b7b077b0814ac0553433afc4bef7fa766e0
Alberto Silva <asilgag@gmail.com>
2021-11-14 10:49:16 +0100

    Refactor cache tests

--- src/lib/utils/cache/__tests__/cache.test.ts
+++ src/lib/utils/cache/__tests__/cache.test.ts
@@ -5,9 +5,20 @@ describe('Cache test', () => {
     expect(cache.bin('bin1').size).toBe(0);
   });
 
+  it('using several cache bins does not affect any of them', () => {
+    cache.bin('bin1').set('key1', 'value1');
+    cache.bin('bin2').set('key2', 'value2');
+
+    expect(cache.bin('bin1').get('key1')).toBe('value1');
+    expect(cache.bin('bin2').get('key2')).toBe('value2');
+    expect(cache.bin('bin1').size).toBe(1);
+    expect(cache.bin('bin2').size).toBe(1);
+  });
+
   it('gets the keys of all cache bins', () => {
     cache.bin('bin1');
     cache.bin('bin2');
+
     expect(cache.keys()).toEqual(['bin1', 'bin2']);
   });
 });
