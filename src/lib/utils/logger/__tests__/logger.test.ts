3afc3d03036815e7de0cbd400d433cd141fa2103
Alberto Silva <asilgag@gmail.com>
2021-11-15 06:30:21 +0100

    Adds first logger test

---
+++ src/lib/utils/logger/__tests__/logger.test.ts
@@ -0,0 +1,12 @@
+import { logger } from '@lib/utils/logger';
+
+describe('Logger utils test', () => {
+  describe('logger', () => {
+    it('is an object with at least four functions: error, warn, info and debug', () => {
+      expect(typeof logger.error).toBe('function');
+      expect(typeof logger.warn).toBe('function');
+      expect(typeof logger.info).toBe('function');
+      expect(typeof logger.debug).toBe('function');
+    });
+  });
+});
