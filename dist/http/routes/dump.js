13121442babe293ee4932f27272d3b9b4ab8720f
Alberto Silva <asilgag@gmail.com>
2022-09-09 12:59:05 +0200

    Renames dump routes

--- dist/http/routes/dump.js
+++ dist/http/routes/dump.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.dumpMetadata = exports.dumpExecuteFull = exports.dumpExecuteIncremental = exports.dumpIndex = void 0;
+exports.dumpMetadata = exports.dumpFull = exports.dumpIncremental = exports.dumpIndex = void 0;
 const fs_1 = __importDefault(require("fs"));
 const dump_1 = require("@lib/store/dump");
 const object_1 = require("@lib/utils/object");
@@ -11,27 +11,27 @@ const config_1 = require("@lib/config");
 const dumpIndex = (req, res) => {
     res.render('statusIndex', {
         links: {
-            '/dump/execute/incremental': 'Execute an incremental dump',
-            '/dump/execute/full': 'Execute a full dump',
+            '/dump/incremental': 'Execute an incremental dump',
+            '/dump/full': 'Execute a full dump',
             '/dump/metadata': 'Dump metadata info',
         },
     });
 };
 exports.dumpIndex = dumpIndex;
-const dumpExecuteIncremental = (req, res) => {
+const dumpIncremental = (req, res) => {
     const dump = dump_1.dumpManager.dump({ incremental: true });
     res.status(200);
     res.set({ 'Content-Type': 'application/json' });
     res.send((0, object_1.jsonify)(dump));
 };
-exports.dumpExecuteIncremental = dumpExecuteIncremental;
-const dumpExecuteFull = (req, res) => {
+exports.dumpIncremental = dumpIncremental;
+const dumpFull = (req, res) => {
     const dump = dump_1.dumpManager.dump({ incremental: false });
     res.status(200);
     res.set({ 'Content-Type': 'application/json' });
     res.send((0, object_1.jsonify)(dump));
 };
-exports.dumpExecuteFull = dumpExecuteFull;
+exports.dumpFull = dumpFull;
 const dumpMetadata = (req, res) => {
     const metadataFilepath = `${config_1.config.dumpDir}/metadata.json`;
     if (fs_1.default.existsSync(metadataFilepath)) {
